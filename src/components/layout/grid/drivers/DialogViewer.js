f7603fb653f33bd5fca7528cc861155fd0083884
Kenny Stewart <120119205+kennystewart@users.noreply.github.com>
2023-06-23 21:31:53 -0400

    Update DialogViewer.js

--- src/components/layout/grid/drivers/DialogViewer.js
+++ src/components/layout/grid/drivers/DialogViewer.js
@@ -28,28 +28,7 @@ export default function OpenDialogImage(props) {
           >
             Dialog title
           </DialogTitle>
-          <DialogContent>
-            {/* <Image src={props.url} width="500px" height="600px" fit="contain"/> */}
-            {file?.webUrl && (fileType == "docx") && 
-            <DocPreview file={file}/>
-            }
-        {file?.webUrl && (fileType =="photo") && 
-            <Image src={file.webUrl} height="500px" width="600px" fit='cover'/>
-        }
-        {file?.webUrl && (fileType == "video") && 
-            <video src={file.webUrl} height="500px" />
-        }
-        {file?.webUrl && (fileType == "audio") && 
-            <audio controls width="100%">
-                <source src={file.webUrl} />
-            </audio>
-        }
-        {file?.webUrl && (fileType == "genericfile") &&
-            <Image src={"/filetypes/" + "unknown.png"} width="500px" height="400px" fit="cover"/>
-        }
-        {file?.webUrl && (fileType != "genericfile" && fileType != "docx" && fileType != "photo" && fileType != "video" ) &&
-            <Image src={`https://spoprod-a.akamaihd.net/files/fabric-cdn-prod_20201008.001/assets/item-types/48/${fileType}.svg`} height="50px" />
-        }
+         ////here is content...
           </DialogContent>
         </DialogBody>
       </DialogSurface>
